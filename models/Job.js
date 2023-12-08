f77b81eaf63b43a7d6be963d500c36cecfa26f56
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2023-12-08 11:36:42 -0500

    feat: implemeted coding assigment for lesson 9: auth login and register, create job (#1)

---
+++ models/Job.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, 'Please provide company name'],
+    maxlength: 50,
+    trim: true,
+  },
+  position: {
+    type: String,
+    required: [true, 'Please provide position'],
+    maxlength: 100,
+    trim: true,
+  },
+  status: {
+    type: String,
+    enum: ['interview', 'declined', 'pending'],
+    default: 'pending',
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please provide user'],
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Job', jobSchema);
