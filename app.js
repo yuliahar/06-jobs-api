5234dc7ef8bc887d4bc39f64db750e6bdb3743e3
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2023-12-10 22:44:12 -0500

    feat: swagger docs added

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -33,6 +38,7 @@ app.use(xss());
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routers
 app.use('/api/v1/auth', authRouter);
