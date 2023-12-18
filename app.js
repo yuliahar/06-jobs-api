aa9764bb8b117408b120ff1bfa0beba64b19f280
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2023-12-18 02:07:48 -0500

    [Lesson 10: Jobs API Part 2] Coding Assignment (#2)
    * feat: completed week 10 code assignment: jobs api
    
    * feat: swagger docs added

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 require('dotenv').config();
 require('express-async-errors');
 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -13,11 +23,22 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routers
 app.use('/api/v1/auth', authRouter);
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,23 +1,32 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
 
-  // General error handling
-  const error = {
-    status: 'error',
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    message: err.message || 'Something went wrong',
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went wrong, try again later...',
   };
 
-  if (process.env.NODE_ENV === 'development') {
-    error.stack = err.stack;
+  if (err.name === 'ValidationError') {
+    customError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+    customError.statusCode = 400;
   }
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+  if (err.code && err.code === 11000) {
+    customError.message = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+
+  if (err.name === 'CastError') {
+    customError.message = `No item found with id : ${err.value}`;
+    customError.statusCode = 404;
+  }
+
+  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+  return res.status(customError.statusCode).json({ msg: customError.message });
 };
 
 module.exports = errorHandlerMiddleware;
-
