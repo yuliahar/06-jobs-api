c05d4d3c98802ded26d0ae4d31627f749cead815
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2024-01-11 17:12:38 -0500

    [Lesson 11: A Front End for the Jobs API] Coding Assignment (#3)
    * feat: completed week 10 code assignment: jobs api
    
    * feat: swagger docs added
    
    * feat: add index.html and js files to public folder to display and work with jobs api
    
    * feat: update app.js
    
    * fix: fix jobId in editing and deleting functionality
    
    * fix: fix deleting job functionality

--- app.js
+++ app.js
@@ -35,10 +35,13 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+// });
+app.use(express.static('public'));
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 
 // routers
 app.use('/api/v1/auth', authRouter);
---
+++ public/addEdit.js
@@ -0,0 +1,149 @@
+import { enableInput, inputEnabled, message, setDiv, token } from "./index.js";
+import { showJobs } from "./jobs.js";
+
+let addEditDiv = null;
+let company = null;
+let position = null;
+let status = null;
+let addingJob = null;
+
+export const handleAddEdit = () => {
+  addEditDiv = document.getElementById("edit-job");
+  company = document.getElementById("jobCompany");
+  position = document.getElementById("position");
+  status = document.getElementById("status");
+  addingJob = document.getElementById("adding-job");
+  const editCancel = document.getElementById("edit-cancel");
+
+  addEditDiv.addEventListener("click", async (e) => {
+    if (inputEnabled && e.target.nodeName === "BUTTON") {
+      if (e.target === addingJob) {
+        enableInput(false);
+
+        let method = "POST";
+        let url = "/api/v1/jobs";
+
+        if (addingJob.textContent === "update") {
+          method = "PATCH";
+          url = `/api/v1/jobs/${addEditDiv.dataset.id}`;
+        }
+
+        try {
+          const response = await fetch(url, {
+            method: method,
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              company: company.value,
+              position: position.value,
+              status: status.value,
+            }),
+          });
+
+          const data = await response.json();
+          if (response.status === 200 || response.status === 201) {
+            if (response.status === 200) {
+              // a 200 is expected for a successful update
+              message.textContent = "The job entry was updated.";
+            } else {
+              // a 201 is expected for a successful create
+              message.textContent = "The job entry was created.";
+            }
+
+            company.value = "";
+            position.value = "";
+            status.value = "pending";
+            showJobs();
+          } else {
+            message.textContent = data.msg;
+          }
+        } catch (err) {
+          console.log(err);
+          message.textContent = "A communication error occurred.";
+        }
+
+        enableInput(true);
+      } else if (e.target === editCancel) {
+        message.textContent = "";
+        showJobs();
+      }
+    }
+  });
+};
+
+export const showAddEdit = async (jobId) => {
+  if (!jobId) {
+    company.value = "";
+    position.value = "";
+    status.value = "pending";
+    addingJob.textContent = "add";
+    message.textContent = "";
+
+    setDiv(addEditDiv);
+  } else {
+    enableInput(false);
+
+    try {
+      const response = await fetch(`/api/v1/jobs/${jobId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+      if (response.status === 200) {
+        company.value = data.company;
+        position.value = data.position;
+        status.value = data.status;
+        addingJob.textContent = "update";
+        message.textContent = "";
+        addEditDiv.dataset.id = jobId;
+
+        setDiv(addEditDiv);
+      } else {
+        // might happen if the list has been updated since last display
+        message.textContent = "The jobs entry was not found";
+        showJobs();
+      }
+    } catch (err) {
+      console.log(err);
+      message.textContent = "A communications error has occurred.";
+      showJobs();
+    }
+
+    enableInput(true);
+  }
+};
+
+export const showDelete = async (jobId) => {
+  enableInput(false);
+  try {
+    const response = await fetch(`/api/v1/jobs/${jobId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    // Check if the response's content type is JSON
+    const data = await response.text();
+    if (response.status === 200) {
+      addingJob.textContent = "delete";
+      message.textContent = data;
+      showJobs();
+    } else {
+      message.textContent = "The jobs entry was not found";
+      showJobs();
+    }
+  } catch (err) {
+    console.error(err);
+    message.textContent = "A communications error has occurred.";
+    showJobs();
+  }
+  enableInput(true);
+};
